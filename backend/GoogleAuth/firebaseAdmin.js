2469769bc30ddfe120fd513b50a7be97249015e1
ankit kumar <ak373714@gmail.com>
2026-02-16 12:40:13 +0530

    Fix firebase admin for production

--- backend/GoogleAuth/firebaseAdmin.js
+++ backend/GoogleAuth/firebaseAdmin.js
@@ -1,26 +1,13 @@
-// import admin from 'firebase-admin';
-// import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
+import admin from "firebase-admin";
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
-// export default admin;
-
-import admin from 'firebase-admin';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const serviceAccountData = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountData),
-   storageBucket: "annpurna-dhaba-fresh-start.firebasestorage.app",
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "annpurna-dhaba-fresh-start.firebasestorage.app",
 });
 
 const bucket = admin.storage().bucket();
-export{bucket};
+
+export { bucket };
 export default admin;
